dd7e5bb69efe355db3a025146c5c9d305924627a
Jacky Hsiung <forlol1123@gmail.com>
2021-05-31 01:11:37 -0400

    style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,38 @@
 import React from 'react'
 import NeonButtonSm from './NeonButtonSm'
 import './Header.css'
-
+import blob from '../assets/img/blob.svg'
+import blob2 from '../assets/img/blob2.svg'
+import blob3 from '../assets/img/blob3.svg'
 export default function Header() {
   return (
-    <div className='header' id='home'>
-      <div className='container container-cln'>
-        <div className='header__name'>I'm Jacky Hsiung</div>
-        <div className="header__title">React Developer</div>
-        <div className='container container-row'>
-          <a className='header__btn' href='#work'>Previous Projects</a>
-          <a className='header__btn' href='#about'>More About Me</a>
+    <>
+      <div className='header' id='home'>
+        <div className='container container-cln header-container'>
+          <div className='header__name'>I'm Jacky Hsiung</div>
+          <div className="header__title">React Developer</div>
+          <div className='container container-row'>
+            <a className='header__btn' href='#work'>Previous Projects</a>
+            <a className='header__btn' href='#about'>More About Me</a>
+          </div>
+
         </div>
+
       </div>
-    </div>
+      {/* <div className='container container-row svg-container'>
+        <img className='header__img svg--pink' src={blob2} />
+        <img className='header__img svg--blue' src={blob} />
+        
+      </div> */}
+      <div className='container--grid svg-container'>
+        <img className='header__img svg--pink' src={blob2} />
+        <img className='header__img svg--blue' src={blob} />
+        <img className='header__img svg--blue' src={blob} />
+        <img className='header__img svg--pink' src={blob3} />
+        <img className='header__img svg--pink' src={blob3} />
+        <img className='header__img svg--pink' src={blob2} />
+      </div>
+
+    </>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,19 @@
 import React from 'react'
 import './Navbar.css'
-
+import logo from '../assets/img/logo.png';
 class Navbar extends React.Component {
   constructor(props) {
     super(props)
-    this.myFunction = this.myFunction.bind(this)
   }
-  myFunction() {
-    console.log('hi')
-    var x = document.getElementById("myTopnav");
-    console.log(x)
-    if (x.className === "topnav") {
 
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
   render() {
     return (
-      <div class="topnav" id="myTopnav">
+      <div class="navbar" id="myTopnav">
+        <img src={logo}/>
         <a href="#home" class="active">Home</a>
         <a href="#work">Work</a>
         <a href="#skills">Skills</a>
         <a href="#about">About</a>
-        <a href="javascript:void(0);" class="icon" onClick={()=>this.myFunction()}>
-          <i class="fa fa-bars"></i>
-        </a>
       </div>
     )
   }
